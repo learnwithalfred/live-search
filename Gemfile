2d625ea97d2727ffb5b45bfa2cfbb4c6b686466f
Alfred Boateng <programmingwithalfred@gmail.com>
2023-04-13 02:36:51 +0000

    setup user mode

--- Gemfile
+++ Gemfile
@@ -70,3 +70,5 @@ end
 
 
 gem 'faker'
+
+gem 'devise'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_13_023017) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_13_023537) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_13_023017) do
     t.text "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name", default: "", null: false
+    t.string "image", default: "https://robohash.org/sitsequiquia.png?size=300x300&set=set5", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
