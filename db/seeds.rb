17bb120da4fd12d5e05c486ed6ba4b5df5239f87
Alfred Boateng <programmingwithalfred@gmail.com>
2023-04-13 03:04:23 +0000

    data seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,23 @@
     title: Faker::Quote.matz
   )
 end
+
+
+User.create!(
+  email: "test@example.com",
+  password: "secrete",
+  password_confirmation: "secrete",
+  name: Faker::Name.name,
+  image: Faker::Avatar.image
+)
+
+
+5.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: "secrete",
+    password_confirmation: "secrete",
+    name: Faker::Name.name,
+    image: Faker::Avatar.image
+  )
+end
