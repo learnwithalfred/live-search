7a6deace9498bc1dd3bacdbd21cbaa36d2ec519e
Alfred Boateng <programmingwithalfred@gmail.com>
2023-04-13 02:40:58 +0000

    render layout dynamically

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  layout :set_layout
+
+  private
+
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name image])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name image])
+  end
+
+  def set_layout
+    if current_user
+      'application'
+    else
+      'auth'
+    end
+  end  
 end
